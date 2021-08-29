615bc88896a0faa86a1851b30d90fe8bb64cf698
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-29 08:41:22 +0530

    removed opacity in mobile size

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -7,13 +7,13 @@ const Features = () => {
             <div>
               
                 <div className="my-8 flex items-center justify-around flex-wrap gap-2">
-                <img src="/TandooriPaneer.jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
-                <img src="/TandooriPaneer1.jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
-                <img src="/TandooriPaneer2.jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
-                <img src="/MaggiChatpata.jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
-                <img src="/HotnSpicy.jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
-                <img src="/Veggies.jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
-                <img src="/SimpleVeg(Capsicum).jpg" className="transition duration-500 ease-in-out opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/TandooriPaneer.jpg" className="transition duration-500 ease-in-out sm:opacity-75 md:opacity-75 lg:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/TandooriPaneer1.jpg" className="transition duration-500 ease-in-out md:opacity-75 lg:opacity-75 sm:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/TandooriPaneer2.jpg" className="transition duration-500 ease-in-out md:opacity-75 lg:opacity-75 sm:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/MaggiChatpata.jpg" className="transition duration-500 ease-in-out md:opacity-75 lg:opacity-75 sm:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/HotnSpicy.jpg" className="transition duration-500 ease-in-out md:opacity-75 lg:opacity-75 sm:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/Veggies.jpg" className="transition duration-500 ease-in-out md:opacity-75 lg:opacity-75 sm:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
+                <img src="/SimpleVeg(Capsicum).jpg" className="transition duration-500 ease-in-out md:opacity-75 lg:opacity-75 sm:opacity-75 hover:opacity-100 transform hover:-translate-y-1 hover:scale-110 cursor-pointer w-40 h-40 object-contain" alt="Pizza" />
                 </div>
                 <img src="/logo.png" alt="logo" className="text-center w-full"/>
             </div>
